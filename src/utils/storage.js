57f6bace5ef68770f548fd16103575f092eb4984
Jake LeBoeuf <dev@jakeleboeuf.com>
2020-06-11 10:57:56 -0400

    Expect user to be a string

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,9 +1,14 @@
 export const isBrowser = () => typeof window !== 'undefined';
 
-export const getUser = (type = 'default') =>
-  isBrowser() && window.localStorage.getItem(`user::${type}`)
-    ? JSON.parse(window.localStorage.getItem(`user::${type}`))
-    : {};
+export const getUser = (type = 'default') => {
+  return isBrowser() &&
+    window.localStorage.getItem(`user::${type}`) &&
+    window.localStorage.getItem(`user::${type}`) !== 'undefined'
+    ? window.localStorage.getItem(`user::${type}`)
+    : null;
+};
 
-export const setUser = ({ type = 'default', user }) =>
-  window.localStorage.setItem(`user::${type}`, JSON.stringify(user));
+export const setUser = ({ type = 'default', user }) => {
+  console.log(user);
+  window.localStorage.setItem(`user::${type}`, user);
+};
